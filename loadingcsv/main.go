78266edcf4e24413f42e5d3d8be9564e7040a2e9
clomollo <oyoocliff471@gmail.com>
2025-03-07 00:38:56 +0300

    Add function that splits the dataset based on the given attribute

--- loadingcsv/main.go
+++ loadingcsv/main.go
@@ -3,88 +3,127 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"math"
 	"os"
 )
 
-
-
 type Data struct {
-	Outlook string
+	Outlook    string
 	Temprature string
-	Humidity string
-	Wind string
+	Humidity   string
+	Wind       string
 	PlayTennis string
 }
 
-
-func LoadCsv(s string)([]string,[]Data,error){
-	file,err :=os.Open(s)
-	if err !=nil{
-		fmt.Println("Error openning file",err)
-		return nil,nil,fmt.Errorf("Error opening file %v",err)
+func LoadCsv(s string) ([]string, []Data, error) {
+	file, err := os.Open(s)
+	if err != nil {
+		fmt.Println("Error openning file", err)
+		return nil, nil, fmt.Errorf("Error opening file %v", err)
 	}
 	defer file.Close()
 
-	reader:=csv.NewReader(file)
+	reader := csv.NewReader(file)
 
-	records,err:=reader.ReadAll()
-	if err !=nil{
-		fmt.Println("Error reading file:",err)
-		return nil,nil,fmt.Errorf("Error reading file:%v",err)
+	records, err := reader.ReadAll()
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+		return nil, nil, fmt.Errorf("Error reading file:%v", err)
 	}
 
 	var dataset []Data
 
-	header:=records[0]
+	header := records[0]
 
-	for _,row:=range records[1:]{
-		dataset = append(dataset,Data{
-			Outlook: row[0],
+	for _, row := range records[1:] {
+		dataset = append(dataset, Data{
+			Outlook:    row[0],
 			Temprature: row[1],
-			Humidity: row[2],
-			Wind: row[3],
+			Humidity:   row[2],
+			Wind:       row[3],
 			PlayTennis: row[4],
 		})
 	}
 
-return header,dataset, nil
+	return header, dataset, nil
 
 }
 
-//CountClass counts the occurrence of the target class in 
-//our dataset
-func countClassOccurrences(dataset []Data) map[string]int{
-	classCounts:=make(map[string]int)
+// CountClass counts the occurrence of the target class in
+// our dataset
+func countClassOccurrences(dataset []Data) map[string]int {
+	classCounts := make(map[string]int)
 
-	for _,row:=range dataset{
+	for _, row := range dataset {
 		classCounts[row.PlayTennis]++
 	}
 
-return classCounts
+	return classCounts
 }
 
 //Calculates probability of each class
 
-func computeProbabilities(classCounts map[string]int, totalSamples int)map[string]float64{
-	probabilities:=make(map[string]float64)
+func computeProbabilities(classCounts map[string]int, totalSamples int) map[string]float64 {
+	probabilities := make(map[string]float64)
 
-	for class,count:=range classCounts{
-		probabilities[class]=float64(count)/float64(totalSamples)
+	for class, count := range classCounts {
+		probabilities[class] = float64(count) / float64(totalSamples)
 	}
 	return probabilities
 }
+//Calculates entropy based on probabilities to determine the impurity of the dataset
+func Entropy(probabilities map[string]float64) float64 {
+	entropy := 0.0
 
-func main(){
-	_,header,err:=LoadCsv("dataset.csv")
-	if err !=nil{
+	for _, probability := range probabilities {
+		if probability > 0 {
+			entropy -= probability * math.Log2(probability)
+		}
+
+	}
+	return entropy
+}
+
+//Split the dataset based on the attribute
+func SplitDataset(dataset []Data, attribute string)map[string][]Data{
+	subsets:=make(map[string][]Data)
+
+	for _,row:=range dataset{
+		var key string
+		switch attribute{
+		case"outlook":
+			key=row.Outlook
+			case "temperature":
+				key=row.Temprature
+			case "humidity":
+				key=row.Humidity
+			case "wind":
+				key=row.Wind
+				
+		}
+		subsets[key]=append(subsets[key],row)
+	}
+	return subsets
+}
+
+func main() {
+	_, header, err := LoadCsv("dataset.csv")
+	if err != nil {
 		fmt.Println("error openning file")
 		return
 	}
 
-	files:=countClassOccurrences(header)
-	totalSamples:=len(header)
-	probabilities:=computeProbabilities(files,totalSamples)
+	files := countClassOccurrences(header)
+	totalSamples := len(header)
+	probabilities := computeProbabilities(files, totalSamples)
+	entropy:=Entropy(probabilities)
+
+	splitted:=SplitDataset(header,"outlook")
+	for key,value:=range splitted{
+		fmt.Printf("%s: %v\n",key,value)
+	}
 	fmt.Println(files)
 	fmt.Println(probabilities)
+	fmt.Printf("%.2f\n",entropy)
 	
 }
